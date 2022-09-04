c8493026b953bac1b82119edd7568c80fe4d8792
Dev Shukla <devshuklaji6@gmail.com>
2022-09-04 17:03:12 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let inputValue = document.querySelector('input')
+let temp = document.querySelector('.temperature')
+let weather = document.querySelector('.weather')
+let description = document.querySelector('.description')
+
+
+const button = document.querySelector('button').addEventListener('click',function(){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=metric&appid=ee86b1de724088bcbde768d27fb1f01e')
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => {
+
+        let tempValue = data['main']['temp']
+        let weatherValue =data['weather'][0]['main']
+        let descriptionValue =data['weather'][0]['description']
+        
+        
+        temp.innerHTML = "Temperature = "+tempValue+" celcius";
+        weather.innerHTML = "Weather = "+weatherValue;
+        description.innerHTML = "Description  = "+descriptionValue;
+        
+    })
+    
+})
+
+
+
